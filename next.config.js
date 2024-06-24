b3a5f154753485a5cd043cb52036d36f73429403
James Ren <jamesren@Jamess-Air.attlocal.net>
2024-06-24 17:46:11 -0500

    deploy to github pages: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     /**
-//    * Enable static exports for the App Router.
-//    *
-//    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-//    */
-//   output: "export",
-
-//   /**
-//    * Set base path. This is the slug of your GitHub repository.
-//    *
-//    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-//    */
-//   basePath: "/upload-image",
-
-//   /**
-//    * Disable server-based image optimization. Next.js does not support
-//    * dynamic features with static exports.
-//    *
-//    * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-//    */
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
+const nextConfig = {
+    basePath: "/upload-image",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
--- src/app/CurrentStack.js
+++ src/app/CurrentStack.js
@@ -8,7 +8,7 @@ export default function CurrentStack( {rows} ) {
     return (
         <Stack className="CURRENT_STACK" overflow={"scroll"} height={539.5} gap={4}>
             {rows.map((row, index) => (
-                <Stack gap={1}>
+                <Stack key={row[0]} gap={1}>
                     <Typography>Card {index+1}</Typography>
                     <Typography sx={{border: 1, borderColor: 'gray', padding:1, borderRadius:"8px"}} overflow={"scroll"} height={150} width={300} alignItems={"center"} justifyContent={"center"}>
                         {row[0]}
